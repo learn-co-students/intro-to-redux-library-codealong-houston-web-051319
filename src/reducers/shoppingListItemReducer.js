f64b2acd9cf361e3bd867ff7ab772af4c851c18b
Stefan Scepanovic <scepa91@icloud.com>
2019-08-01 15:13:37 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,18 @@
 export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
+  state = {
+    items: []
+  },
+  action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+  console.log(action);
 
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return Object.assign({}, state, {
+        items: state.items.concat(state.items.length + 1)
+      });
+
+    default:
+      return state;
+  }
 }
