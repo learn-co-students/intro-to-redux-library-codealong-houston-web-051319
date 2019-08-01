a1b52c0ee1f74b6e65db5578d9a20e38ca98106a
feven tsegay <feventsegay6@gmail.com>
2019-08-01 16:06:09 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,7 +6,13 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+			// return Object.assign({}, state, {
+        	// 	items: state.items.concat(state.items.length + 1)
+			  // });
+			  return {
+				  ...state,
+				  items: [ ...state.items, state.items.length + 1 ]
+			  }
 
 		default:
 			return state;
